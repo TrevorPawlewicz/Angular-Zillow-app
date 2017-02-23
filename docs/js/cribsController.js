672e6a1ce25ad0d6017fc6834ac533869e8851f2
Trev <trevor.pawlewicz@gmail.com>
2017-02-23 14:16:31 -0500

    update Controller with add function

--- docs/js/cribsController.js
+++ docs/js/cribsController.js
@@ -6,10 +6,41 @@ angular.module('ngCribs')
 
         $scope.cribs; // for cribs data
 
+        // default setting:
         $scope.priceInfo = {
             min: 0,
             max: 1000000
-        }
+        };
+
+        // new listing added:
+        $scope.newListing = {};
+
+        // ADD
+        $scope.addCrib = function(newListing) {
+			newListing.image = 'default-crib';
+			$scope.cribs.push(newListing);
+			$scope.newListing = {}; // clear input boxes
+		}
+
+        // EDIT
+        $scope.editCrib = function(crib) {
+			$scope.editListing = true;
+			$scope.existingListing = crib;
+		}
+
+        // UPDATE
+		$scope.saveCribEdit = function() {
+			$scope.existingListing = {};
+			$scope.editListing = false;
+		}
+
+        // DELETE
+		$scope.deleteCrib = function(listing) {
+			var index = $scope.cribs.indexOf(listing);
+			$scope.cribs.splice(index, 1);
+			$scope.existingListing = {};
+			$scope.editListing = false;
+		}
 
         cribsFactory.getCribs().success(function(data){
             $scope.cribs = data;
